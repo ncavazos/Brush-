f75763ae0ef4084b722083343efe9c0dcc7cfb04
cata <ncavazos18@gmail.com>
2022-10-19 21:21:42 -0500

    Add files via upload

---
+++ brushillator.js
@@ -0,0 +1,20 @@
+setFunction({
+  name: 'brush',
+  type: 'src',
+  inputs: [
+    {
+      type: 'float',
+      name: 'frequency',
+      default: 0,
+    },
+    {
+      type: 'float',
+      name: 'sync',
+      default: .1,
+    },
+  ],
+  glsl: `   vec2 st = _st;
+  
+  
+   return vec4( tan(st.x*3.14159*frequency), sin(st.x*3.14159*frequency),tan(st.x*3.14159*frequency), 1.0);`
+})
